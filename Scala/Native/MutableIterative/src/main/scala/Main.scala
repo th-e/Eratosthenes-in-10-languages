5ecaa376d21716131c9a277d116c750fee57cecb
Thomas Effing <dihedral3@yahoo.de>
2020-02-17 20:56:06 +0100

    scala native a little bit faster

--- Scala/Native/MutableIterative/src/main/scala/Main.scala
+++ Scala/Native/MutableIterative/src/main/scala/Main.scala
@@ -1,17 +1,18 @@
 import scala.math.sqrt
 import scala.collection.mutable.MutableList
+import scala.collection.mutable.ListBuffer
 
 object Main {
 
-  def findPrimes(n: Int): MutableList[Int] = {
+  def findPrimes(n: Int): ListBuffer[Int] = {
     var sieve = new Array[Boolean](n)
-    var primes = MutableList.empty[Int]
+    var primes =  new ListBuffer[Int]
     val sqrtN: Int = sqrt(n.toDouble).toInt
 
-    primes = primes :+ 2
+    primes = primes += 2
     for (i <- 3 until n by 2) {
       if (!sieve(i)) {
-        primes += i
+        primes = primes += i
         if (i < sqrtN) {
           for (j <- i * i until n by 2 * i) {
             sieve.update(j, true)
